ff9e59370c20dcbb3d0396ae50a34abad3d1f8fe
Maxi Sandoval <maxisandoval98@gmail.com>
2025-01-02 18:54:37 -0300

    added client basic config

---
+++ auth2-client-app/src/main/java/ar/dev/maxisandoval/auth2clientapp/Auth2ClientAppApplication.java
@@ -0,0 +1,13 @@
+package ar.dev.maxisandoval.auth2clientapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth2ClientAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Auth2ClientAppApplication.class, args);
+    }
+
+}
---
+++ auth2-client-app/src/test/java/ar/dev/maxisandoval/auth2clientapp/Auth2ClientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.dev.maxisandoval.auth2clientapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth2ClientAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
