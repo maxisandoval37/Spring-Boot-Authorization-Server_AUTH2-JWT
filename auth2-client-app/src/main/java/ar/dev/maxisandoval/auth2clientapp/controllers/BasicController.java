616663818f34e89288f636c31739b540462a36c9
Maxi Sandoval <maxisandoval98@gmail.com>
2025-01-02 23:03:37 -0300

    added controllers

---
+++ auth2-client-app/src/main/java/ar/dev/maxisandoval/auth2clientapp/controllers/BasicController.java
@@ -0,0 +1,30 @@
+package ar.dev.maxisandoval.auth2clientapp.controllers;
+
+import ar.dev.maxisandoval.auth2clientapp.models.Message;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
+@Slf4j
+public class BasicController {
+
+    @GetMapping("/listMessages")
+    public List<Message> listMessages() {
+        log.info("Enter to: listMessages");
+        return Collections.singletonList(new Message("Test"));
+    }
+
+    @PostMapping("/createMessage")
+    public Message createMessage(@RequestBody Message message) {
+        log.info("Message saved: ".concat(message.getText()));
+        return message;
+    }
+
+    @GetMapping("/authorized")
+    public Map<String, String> authorized(@RequestParam String code) {
+        log.info("Authorized code: ".concat(code));
+        return Collections.singletonMap("code", code);
+    }
+
+}
